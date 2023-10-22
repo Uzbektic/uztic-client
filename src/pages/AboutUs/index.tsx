8c389289c0930923aa45043b4490ce7c30e5aeba
Gohar Ashraf <35106992+imgohar@users.noreply.github.com>
2023-10-22 18:05:04 +0500

    Extra text removed

--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -14,22 +14,7 @@ const AboutUs = () => {
   Tourists who are interested in visiting Ziyarats in Uzbekistan for them our office can organise a complete comprehensive tour covering all Ziyarats in different cities. Starting from Tashkent, Termez, Kitab, Samarkand, Bukhara and Khiva. We can also arrange tours according to your demand and price.
   <br /><br />
   Having more than 20 years of experience working with Uzbekistan our office is highly recommended not only by the Uzbekistan Embassy in Islamabad but also by high officials in Uzbekistan.
-  We are having offices in different cities of Pakistan listed under 
-  <br /><br />
-  <strong>Uzbekistan Tourist Information Center Head Office.</strong>
-  <br />
-  55 Silk Tower, Civic Center Bahria Town Phase 4 Islamabad. <strong>Tel</strong> +92 300 9745458 & +92 300 9745456, <strong>Email</strong>: <a href="mailto:tic.uzbek@gmail.com" style="color: steelblue" target="_blank">tic.uzbek@gmail.com</a>
-  <br /><br />
-  <strong>Lahore Office </strong>
-  36-1-C1, College Road Township Lahore Tel: +92 303 4888844
-  <br /><br />
-  <strong>Karachi Office.</strong>
-  CL 5/6, Ground Floor Shafi Court, Merewether Road, Civil Lines Karachi Tel: +92 21 35215144-54-97
-  <br /><br />
-  <strong>South Punjab Office</strong>
-  Abbasia Banglose opposite Zap School Rahim Yaar Khan Tel: +92 314 6703390
-  <br /><br />
-  We are working hard to serve you better and looking forward to seeing you in Uzbekistan. For any query or information please call our office in your area. We will be happy to provide you with all kinds of help/Services.`;
+  We are having offices in different cities of Pakistan listed under`;
 
   const slideImages = [
     {
