ecf422f5c9e3cb274a4461a9daba4ff414b55402
Gohar Ashraf <35106992+imgohar@users.noreply.github.com>
2023-12-03 16:11:27 +0500

    new change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HashRouter } from 'react-router-dom';
 import RouteList from './routes';
 import { ThemeProvider } from '@mui/material';
 import { theme } from './theme';
+import { Mixpanel } from './services/mixpanel';
 
 function App() {
+  useEffect(() => {
+    console.log('useEffect is running');
+    Mixpanel.track_pageview();
+  }, []);
+
   return (
     <HashRouter basename="/">
       <ThemeProvider theme={theme}>
---
+++ src/services/mixpanel.tsx
@@ -0,0 +1,5 @@
+import mixpanel from 'mixpanel-browser';
+
+mixpanel.init('9b989904a8badca0d2760a0f2763b464');
+
+export let Mixpanel = mixpanel;
